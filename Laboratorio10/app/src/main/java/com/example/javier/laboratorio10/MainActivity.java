5a9e9eca39acf73b746f9daa761df1fbd88a850b
DESKTOP-J61MO7B\jaiba <jaibarra1@miuandes.cl>
2018-06-05 11:55:21 -0400

    lab10

---
+++ Laboratorio10/app/src/main/java/com/example/javier/laboratorio10/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.javier.laboratorio10;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    String url = "http://demo.tutorialzine.com/2012/04/mobile-touch-gallery/";
+    WebView web;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        web = (WebView) findViewById(R.id.id_webview);
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setBuiltInZoomControls(true);
+        web.loadUrl(url);
+
+        web.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView webView,String url){
+                return false;
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack()) {
+            web.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
